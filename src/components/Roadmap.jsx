be7210c247bd17c3b2b92cb2875e79ef1dbaa7a2
calavanda <ninguang.01@gmail.com>
2025-02-21 00:45:45 -0600

    2

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,9 +1,7 @@
 import Button from "./Button";
-import ButtonSvg from "../assets/svg/ButtonSvg";
-import ButtonGradient from "../assets/svg/ButtonGradient";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./TagLine";
+import Tagline from "./Tagline";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
